692fe0edf4d901207ff1d7729d2eb9237e3e49c6
arthurnov <art.dsky@gmail.com>
2024-08-19 23:47:32 +0300

    initial ajax

--- script.js
+++ script.js
@@ -2,6 +2,23 @@
 
 (() => {
 
+    const searchButton = document.getElementById("search-button"); 3
+    searchButton.addEventListener("click", () => { getData("text passed") });
 
+    if (JSON.parse(localStorage.getItem("coins-list")) === null) {
+        localStorage.setItem("coins-list", JSON.stringify([]));
+    };
+
+
+
+    function getData(text) {
+        const options = { method: 'GET', headers: { accept: 'application/json' } };
+
+        fetch('https://api.coingecko.com/api/v3/coins/markets?order=market_cap_desc&vs_currency=usd#', options)
+            .then(response => response.json())
+            .then(response => console.log(response))
+            .catch(err => console.error(err));
+    }
 
 })();
+
