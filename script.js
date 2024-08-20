17a8f77bd6dcc4f17e910cc097b62490c12a89ed
arthurnov <art.dsky@gmail.com>
2024-08-20 15:19:09 +0300

    basic data interaction implemented

--- script.js
+++ script.js
@@ -2,23 +2,35 @@
 
 (() => {
 
-    const searchButton = document.getElementById("search-button"); 3
-    searchButton.addEventListener("click", () => { getData("text passed") });
+    const searchButton = document.getElementById("search-button");
+    searchButton.addEventListener("click", () => getData());
 
+    // create empty object in local storage in case of a read from it before data is loaded in.
     if (JSON.parse(localStorage.getItem("coins-list")) === null) {
         localStorage.setItem("coins-list", JSON.stringify([]));
     };
 
+    // get data from api
+    async function getData() {
+        const options = { method: 'GET', headers: { accept: 'application/json' } }; // copy-paste code example from coingecko
 
-
-    function getData(text) {
-        const options = { method: 'GET', headers: { accept: 'application/json' } };
-
-        fetch('https://api.coingecko.com/api/v3/coins/markets?order=market_cap_desc&vs_currency=usd#', options)
+        await fetch('https://api.coingecko.com/api/v3/coins/markets?order=market_cap_desc&vs_currency=usd#', options)
             .then(response => response.json())
-            .then(response => console.log(response))
+            .then(response => saveData(response))
             .catch(err => console.error(err));
     }
+
+    // save data to local storage
+    function saveData(data) {
+        localStorage.setItem("coins-list", JSON.stringify(data));
+    }
+
+    // load data from local storage
+    function loadData() {
+        return JSON.parse(localStorage.getItem("coins-list"));
+    }
+
+    console.log(loadData());
 
 })();
 
