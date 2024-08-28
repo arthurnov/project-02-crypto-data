696084d1ba682838dad6ce808cc55dc73256f9dd
arthurnov <art.dsky@gmail.com>
2024-08-28 21:31:20 +0300

    added coin caching

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 "use strict";
 
-(() => {
+(async () => {
 
     // -------------------- INITIAL SETUP --------------------
 
@@ -19,15 +19,19 @@
     aboutLink.addEventListener("click", () => drawAbout());
     let moreInfo = [];
 
+
     searchButton.addEventListener("click", () => search(searchTextField.value));
     searchTextField.addEventListener("input", () => search(searchTextField.value));
 
     // if first page load - get coins from api
     if (JSON.parse(localStorage.getItem("coins-list")) === null) {
         //localStorage.setItem("coins-list", JSON.stringify([]));
-        getData();
+        await getData();
+        drawCards(loadData());
+    } else {
+        drawCards(loadData());
     };
-    drawCards(loadData());
+
 
     // -------------------- SEARCH --------------------
 
@@ -49,10 +53,10 @@
 
     // display search results
     function search(queryString) {
-        // if (Date.now() - timestamp > 120000) {
-        //     getData();
-        //     timestamp = Date.now();
-        // }
+        if (Date.now() - timestamp > 120000) {
+            getData();
+            timestamp = Date.now();
+        }
         const results = searchData(queryString);
         drawCards(results, queryString);
     }
@@ -82,18 +86,18 @@
     }
 
     // get data from api
-    function getData() {
+    async function getData() {
         const options = { method: 'GET', headers: { accept: 'application/json' } }; // copy-paste code example from coingecko
-        fetch('https://api.coingecko.com/api/v3/coins/markets?order=market_cap_desc&vs_currency=usd#', options)
+        await fetch('https://api.coingecko.com/api/v3/coins/markets?order=market_cap_desc&vs_currency=usd#', options)
             .then(response => response.json())
             .then(response => saveData(response))
             .catch(err => console.error(err));
     }
 
     // get coin specific data from api
-    function getCoinData(id) {
+    async function getCoinData(id) {
         const options = { method: 'GET', headers: { accept: 'application/json' } }; // copy-paste code example from coingecko
-        fetch(`https://api.coingecko.com/api/v3/coins/${id}?localization=false?tickers=false&community_data=false&developer_data=false&sparkline=false`, options)
+        await fetch(`https://api.coingecko.com/api/v3/coins/${id}?localization=false?tickers=false&community_data=false&developer_data=false&sparkline=false`, options)
             .then(response => response.json())
             .then(response => saveCoinData(response))
             .catch(err => console.error(err));
@@ -105,14 +109,34 @@
     function getInfoButtons() {
         moreInfo = document.getElementsByClassName("coin-info-button");
         for (let item of moreInfo) {
-            item.addEventListener("click", () => getCoinData(item.id))
+            item.addEventListener("click", () => showMoreInfo(item.id))
         }
+    }
+
+    async function showMoreInfo(id) {
+        let moreInfoList = new Map((localStorage.getItem("more-info-list") === null) ? [] : JSON.parse(localStorage.getItem("more-info-list")));
+        // if id not in list or its been more than 2 minutes since last api call for id - call api.
+        if (!moreInfoList.has(id) || Date.now() - moreInfoList.get(id) > 120000) {
+            moreInfoList.set(id, Date.now());
+            await getCoinData(id);
+        }
+        localStorage.setItem("more-info-list", JSON.stringify([...moreInfoList]));
+
+        const coin = loadCoinData(id);
+
+        console.log(coin);
+        console.log("image link: ", coin["image"]["large"]);
+        console.log("coin name: ", coin["name"]);
+        console.log("USD: ", coin["market_data"]["current_price"]["usd"]);
+        console.log("EUR: ", coin["market_data"]["current_price"]["eur"]);
+        console.log("ILS: ", coin["market_data"]["current_price"]["ils"]);
     }
 
     // -------------------- HOMEPAGE MANIPULATION --------------------
 
     // "home" page
     function drawCards(data, queryString) {
+        document.title = "Crypto-Graph - Home";
         // clear screen
         mainArea.textContent = '';
 
@@ -137,7 +161,7 @@
             <div class="coin-symbol">${element["symbol"]}</div>
             <div class="coin-name">${element["name"]}</div>
             <div class="coin-info"><button class="coin-info-button" id="${element["id"]}">More Info</button></div>
-            <input type="checkbox" class="coin-check" id="${element["symbol"]}-check">`;
+            <input type="checkbox" class="coin-check" id="${element["id"]}-check">`;
             cardsDiv.appendChild(coinDiv);
         });
 
@@ -147,19 +171,21 @@
         } else {
             countDiv.innerText = `Showing ${count} results for "${queryString}"`;
         }
-        
+
         getInfoButtons();
 
     }
 
     // "live reports" page
     function drawGraph() {
+        document.title = "Crypto-Graph - Live Reports";
         // clear screen
         mainArea.textContent = '';
     }
 
     // "about" page
     function drawAbout() {
+        document.title = "Crypto-Graph - About";
         // clear screen
         mainArea.textContent = '';
     }
